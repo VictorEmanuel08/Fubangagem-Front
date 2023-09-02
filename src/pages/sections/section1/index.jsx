f556c551780cf056865105ad65f6d210b190eaaa
VictorEmanuel08 <victorem3181@gmail.com>
2023-09-02 19:31:39 -0300

    folders

---
+++ src/pages/sections/section1/index.jsx
@@ -0,0 +1,16 @@
+import "./style.scss";
+
+export function Home() {
+  return (
+    <div className="home">
+      <header>header</header>
+      <div className="sections">
+        <section>1</section>
+        <section>2</section>
+        <section>3</section>
+        <section>4</section>
+      </div>
+      <footer>footer</footer>
+    </div>
+  );
+}
